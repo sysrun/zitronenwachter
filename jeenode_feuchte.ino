ed17d2282fdefddb9179f83f5cb6f3e2b16d5115
Frederik Granna <frederik@tape.io>
2012-12-09 16:04:07 +0100

    Added DHT11 Sensor on PORT1

--- jeenode_feuchte.ino
+++ jeenode_feuchte.ino
@@ -4,41 +4,66 @@
 
 #include <JeeLib.h>
 #define DEBUG true
-    
+#define NODEID 11
+#define GROUP 212
 Port sensor1 (3);
+DHTxx dht (4); // PORT1,D
+
 MilliTimer report;
+MilliTimer dhtRead;
 
 struct {
     uint16_t sensor1;     // light sensor
     uint16_t sensor2;  // motion detector
+    uint16_t temp;
+    uint16_t humi;
     byte lobat :8;  // supply voltage dropped under 3.1V
 } payload;
 
 // has to be defined because we're using the watchdog for low-power waiting
-ISR(WDT_vect) { Sleepy::watchdogEvent(); }
+//ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
 void setup () {
     #if DEBUG
     Serial.begin(57600);
     Serial.println("\n[jeenode_feuchte]");
     #endif
-    rf12_initialize(11, RF12_868MHZ, 212);
+    rf12_initialize(NODEID, RF12_868MHZ, GROUP);
 }
 int wert1;
+int t,h;
 void loop () {
   wert1 = sensor1.anaRead();
+  
+  // Read DHT11 Sensor
+  if (dhtRead.poll(1200)) {
+    if (dht.reading(t, h)) {
+      payload.temp = t;
+      payload.humi = h;
+    }
+  }
+  
+  // Send Payload
   if (report.poll(1000)) {
     #if DEBUG
-      Serial.println(wert1);
+      Serial.print(wert1);
+      Serial.print(" - T:");
+      Serial.print((payload.temp/10));
+      Serial.print(" - H:");
+      Serial.print((payload.humi/10));
+      Serial.print("%");
+      Serial.println();
     #endif
     payload.sensor1 = wert1;
     payload.sensor2 = wert1;
     payload.lobat = rf12_lowbat();
 
-    rf12_sleep(RF12_WAKEUP);
+    //rf12_sleep(RF12_WAKEUP);
+    
     while (!rf12_canSend())
       rf12_recvDone();
-    rf12_sendStart(0, &payload, sizeof payload, 2);
-    rf12_sleep(RF12_SLEEP);
+    rf12_sendStart(0, &payload, sizeof payload);
+    delay(10);
+    //rf12_sleep(RF12_SLEEP);
   }
 }
